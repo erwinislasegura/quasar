ed95e6b892b8e2ff493ced3726bbf2c5b048afed
erwinislasegura <erwin.2785@gmail.com>
2026-08-07 02:08:58 -0400

    Separar métricas del dashboard en tres gráficos

--- app/Views/layouts/admin.php
+++ app/Views/layouts/admin.php
@@ -7,7 +7,7 @@
   <meta name="theme-color" content="#102a4c">
   <link rel="manifest" href="<?= url('manifest.webmanifest') ?>">
   <title><?= e($title ?? 'Quasar') ?> · Analítica Local</title>
-  <link rel="stylesheet" href="<?= url('assets/css/admin.css') ?>?v=20260802-14">
+  <link rel="stylesheet" href="<?= url('assets/css/admin.css') ?>?v=20260807-1">
   <link rel="stylesheet" href="<?= url('assets/css/responsive.css') ?>?v=20260802-2">
 </head>
 <body>
@@ -26,7 +26,7 @@
   <div class="tooltip" id="tooltip"></div>
   <?php if (!empty($dashboardScripts)): ?>
     <script>window.QUASAR_DATA = <?= json_encode($records, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_HEX_TAG) ?>;</script>
-    <script src="<?= url('assets/js/dashboard.js') ?>?v=20260802-10"></script>
+    <script src="<?= url('assets/js/dashboard.js') ?>?v=20260807-1"></script>
   <?php else: ?>
     <script src="<?= url('assets/js/admin.js') ?>?v=20260802-2"></script>
   <?php endif; ?>
